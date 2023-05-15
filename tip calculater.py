39318ebae1af77bd6af4eca9e85732f9fcc082fe
nokill0054 <44005985+nokill0054@users.noreply.github.com>
2023-05-15 10:23:50 +0300

    tip calculater.py

--- tip calculater.py
+++ tip calculater.py
@@ -8,6 +8,7 @@ tip = int(input("How much tip would you like to give? 10, 12, or 15?\n"))
 people_bill = int(input("Each person should pay\n"))
 
 people_bill1 = round(float((bill+((bill*tip)/100))/people_bill),2)
+people_bill1 = "{:.2f}".format(people_bill1)
 print(f"Each person should pay: ${people_bill1}")               
 
 
