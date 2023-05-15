b238d754a1a4f4f718057fdae5067dd61fef98c9
nokill0054 <44005985+nokill0054@users.noreply.github.com>
2023-05-15 09:45:42 +0300

    Add files via upload

---
+++ YourLifeInWeeks.py
@@ -0,0 +1,18 @@
+#Life in Weeks#
+#1 year 365 days
+#1 year 52 week
+#1 year 12 months
+
+days=int(365)
+weeks=int(52)
+months=int(12)
+
+age =int(input("What is your current age? \n"))
+      
+days = int(age)*int(days)
+
+weeks = int(age)*int(weeks)
+
+months = int(age)*int(months)
+
+print(f"You have {days} days,{weeks} weeks and {months}months left")
